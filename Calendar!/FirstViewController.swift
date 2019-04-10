000eacde21ada5e71259cf3c23f85e5538238eb1
Yongkang Lin <Yongkang.lin2002@gmail.com>
2019-04-10 09:01:28 -1000

    Attached "+" button to code, requires testing and readjustments

--- Calendar!/FirstViewController.swift
+++ Calendar!/FirstViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Mark:Properties
+    
+    @IBAction func AddTextBox(_ sender: UIButton) {
+        func CreateBox() {
+            super.viewDidLoad()
+            
+            let Boxes =  UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
+            Boxes.placeholder = "Enter Text"
+            Boxes.font = UIFont.systemFont(ofSize: 12)
+            Boxes.borderStyle = UITextField.BorderStyle.roundedRect
+            Boxes.autocorrectionType = UITextAutocorrectionType.no
+            Boxes.keyboardType = UIKeyboardType.default
+            Boxes.returnKeyType = UIReturnKeyType.done
+            Boxes.clearButtonMode = UITextField.ViewMode.whileEditing
+            Boxes.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+            Boxes.delegate = self
+        }
     }
-    
-    
 }
