e3b4aed337754c3b9315485038335a8bc11ac358
Alaysia Navor <alaysia.navor@gmail.com>
2019-04-11 01:42:38 -1000

    added table view, "to do" label, also included title name if we are to add a navigation controller in future

--- Calendar!/FirstViewController.swift
+++ Calendar!/FirstViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate, UITextFieldDelegate {
+    @IBOutlet weak var table: UITableView!
+    
+    var data:[String]=["Row 1", "Row 2", "Row 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Use the following if we add a navigation controller to name tab:
+                self.title = "To Do List"
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    
+    
     
     //Mark:Properties
     
