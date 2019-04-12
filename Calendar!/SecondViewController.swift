f646a01df39668f995153015bdfefc0f334381c4
Yongkang Lin <Yongkang.lin2002@gmail.com>
2019-04-12 08:54:28 -1000

    code

--- Calendar!/SecondViewController.swift
+++ Calendar!/SecondViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBAction func Add(_ sender: UIButton) {
+        let someFrame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 30.0)
+        
+        let text = UITextField(frame: someFrame)
+        let view = UIView(frame: someFrame)
+        view.addSubview(text)
+    }
+    
+    var textFields: [UITextField] = []
+    let textFieldSize = CGSize(width: 97, height: 30)
+    let maxTextFields = 4
+    
+    func tappedButton(button: UIButton) {
+        if textFields.count < maxTextFields {
+            let y = CGFloat(textFields.count) * textFieldSize.height
+            let textField = UITextField(frame: CGRect(origin: CGPoint(x: 0, y: y), size: textFieldSize))
+            view.addSubview(textField)
+            textFields.append(textField)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
