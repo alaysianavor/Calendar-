2ef087d2ed11447bdce15d27334dd552402905a6
Alaysia Navor <alaysia.navor@gmail.com>
2019-04-15 08:57:45 -1000

    Yong's credit

--- Calendar!/FirstViewController.swift
+++ Calendar!/FirstViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBAction func Add(_ sender: UIBarButtonItem) {
+        let Textfield = UITextField(frame: CGRect(x: 0, y: 0, width: 300.00, height: 30.00));
+   
+    Textfield.center=self.view.center
+    Textfield.placeholder="Place holder text"
+    Textfield.text="UItextfield example"
+    Textfield.borderStyle=UITextField.BorderStyle.line
+    Textfield.backgroundColor=UIColor.white
+    Textfield.textColor=UIColor.blue
+        
+    self.view.addSubview(Textfield)
+    
+    
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func addNewItem(_ sender: Any) {
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
 }
