fe9b1c753fd10d3aa5db797c7bf1d138db615d0e
Yongkang Lin <Yongkang.lin2002@gmail.com>
2019-04-15 09:28:32 -1000

    ToDoMain.swift

---
+++ Calendar!/ToDoMain.swift
@@ -0,0 +1,11 @@
+//
+//  ToDoMain.swift
+//  Calendar!
+//
+//  Created by Yongkang Lin on 4/15/19.
+//  Copyright © 2019 Alaysia & Yong. All rights reserved.
+//
+
+import Foundation
+
+var ToDoData:[String] = ["PLACEHOLDER DATA", "PLACEHOLDER DATA 2"]
