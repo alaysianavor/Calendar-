3fa4966064d86554efdb6bbc2ab1db09e7218424
Yongkang Lin <Yongkang.lin2002@gmail.com>
2019-04-15 08:22:22 -1000

    a lot of work

--- Calendar!/FirstViewController.swift
+++ Calendar!/FirstViewController.swift
@@ -8,49 +8,8 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UITableViewDataSource {
-    @IBOutlet weak var table: UITableView!
-
-    var data:[String]=["Row 1", "Row 2", "Row 3"]
-    
+class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // Use the following if we add a navigation controller to name tab:
-               self.title = "To Do List"
-    //creates add/plus button
-        let addBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addRow))
-        self.navigationItem.rightBarButtonItem = addBarButton
-        
-        //
-        
-        
     }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!;        cell.textLabel?.text = data[indexPath.row]
-        return cell
-    }
-    
-   //code that is execusted when plus button is pressed
-    @objc func addRow() {
-        let name:String = "Row \(data.count + 1)"
-        data.insert(name, at: 0)
-        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
-        table.insertRows(at: [indexPath], with: .automatic)
-    }
-    
-    
-    //Mark:Properties
-    
-   
-        }
-
-// lynda
-
-
+}
