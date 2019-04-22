d896c8db7bde9b61ce670a4ece71196f965e93d0
Alaysia Navor <alaysia.navor@gmail.com>
2019-04-21 21:43:34 -1000

    deleted calendar stuff

--- Calendar!/AppDelegate.swift
+++ Calendar!/AppDelegate.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Alaysia & Yong. All rights reserved.
 //
 
-import UIKit
+/* import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -43,4 +43,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
+*/
+
+ import UIKit
+ import CoreData
+ 
+ @UIApplicationMain
+ class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
+ var window: UIWindow?
+ 
+ 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+ // Override point for customization after application launch.
+ return true
+ }
+ 
+ func applicationWillTerminate(_ application: UIApplication) {
+ self.saveContext()
+ }
+ 
+ // MARK: - Core Data stack
+ 
+ lazy var persistentContainer: NSPersistentContainer = {
+ let container = NSPersistentContainer(name: "DataModel")
+ container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+ if let error = error as NSError? {
+ fatalError("Unresolved error \(error), \(error.userInfo)")
+ }
+ })
+ return container
+ }()
+ 
+ // MARK: - Core Data Saving support
+ 
+ func saveContext () {
+ let context = persistentContainer.viewContext
+ if context.hasChanges {
+ do {
+ try context.save()
+ } catch {
+ let nserror = error as NSError
+ fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+ }
+ }
+ }
+ 
+ 
+ }
+ 
 
--- Calendar!/FirstViewController.swift
+++ Calendar!/FirstViewController.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2019 Alaysia & Yong. All rights reserved.
 //
 
-import UIKit
+//import UIKit
 
-class FirstViewController: UITableViewController {
+/*class FirstViewController: UITableViewController {
     @IBAction func Add(_ sender: UIBarButtonItem) {
         let Textfield = UITextField(frame: CGRect(x: 0, y: 0, width: 300.00, height: 30.00));
     Textfield.placeholder="Enter item here"
@@ -43,4 +43,102 @@ class FirstViewController: UITableViewController {
     
     }
 }
+} */
+
+
+import UIKit
+import CoreData
+
+class TodoeyViewController: UITableViewController {
+    
+    
+    var array = [TodoeyModel]()
+    var selectedCategory: Category?{
+        didSet{
+            loadItems()
+        }
+    }
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        tableView.rowHeight = 70
+    }
+    
+    //MARK: - TableView methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row].title
+        
+        cell.accessoryType = array[indexPath.row].done == true ? .checkmark : .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        array[indexPath.row].done = !array[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK: - Add button
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add new", style: .default) { (action) in
+            let newItem = TodoeyModel(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+            self.array.append(newItem)
+            self.saveItems()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+   
+    //MARK: - Save and Load from Persistent Cntainer
+    func saveItems() {
+        do{
+            try context.save()
+        }catch{
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<TodoeyModel> = TodoeyModel.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        }else{
+            request.predicate = categoryPredicate
+        }
+        
+        do{
+            array = try context.fetch(request)
+        }catch{
+            print("Error in fetching: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    
+    
 }
+
+ 
